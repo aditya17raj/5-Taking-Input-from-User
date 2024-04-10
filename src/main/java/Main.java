db1a8b6ff8965f099341bb33f3de3855ea580cab
Aditya <141512093+aditya17raj@users.noreply.github.com>
2024-04-10 15:50:52 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,30 @@
+//M E T H O D :- 1 using Scanner class
+
+// import java.util.Scanner;
+// public class Main {
+//   public static void main(String[] args) {
+//     Scanner sc=new Scanner(System.in);
+    
+//     int a,b;
+//     System.out.println("Enter two numbers");
+//     a=sc.nextInt();
+//     b=sc.nextInt();
+//     System.out.println("Sum of two numbers is "+(a+b));
+//   }
+// }
+
+//M E T H O D :- 2 using BufferedReader 
+
+// import java.io.*;
+// public class Main{
+//   public static void main(String[] args) throws IOException{
+//     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+//     int a,b;
+//     System.out.println("Enter two numbers");
+//     a=Integer.parseInt(br.readLine());
+//     b=Integer.parseInt(br.readLine());
+
+//     System.out.println("sum of two number is = "+ (a+b));
+//   }
+// }
